de9028739ee5ff6eb06b17b113abd5c1cf2233d9
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:41:49 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,151 @@
+<?php include "header.php" ?>
+<div class="page-heading" style="background-image:url('image/winningimg14.jpg'); height: 405px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" style="text-align: center; padding: 50px;">
+                <span class="demo"><a href="#">HOME</a> / ABOUT US</span>
+                <div class="heading">
+                <h3>About Us</h3>
+                <h6>GROW STRONG TO TAKE UP THE CHALLENGES
+                    OF LIFE.
+                </h6>
+                <h6>THE TOP LESSONS FOR GETTING YOU BACK ON TRACK.</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- ABOUT WINNING WAVEZ -->
+<section>
+<div class="container py-5">
+        <div class="text-center mx-auto mb-5" style="max-width: 600px;">
+            <h1 class="display mb-0 mt-5" style="font-weight: bold;">ABOUT WINNING WAVEZ</h1>
+            <hr class="w-25 mx-auto bg-primary">
+        </div>
+        <p class="para">WinningWavez is an e-learning
+            platform. We are a company with values for life,
+            envisioning a future and a mission of growth. We believe that each and everyone on the earth is blessed with
+            equal potential, and so they must get the equal opportunity to grow as well.
+            Our mission is to empower individuals to achieve financial independence and improve their lives through
+            acquiring new skills, earning money online and becoming a part of our community of successful entrepreneurs.
+            We provide access to expert instruction, resources, and support to help our clients develop the skills and
+            knowledge they need to succeed in the digital economy, and constantly strive for excellence in everything we
+            do. We want to bring the digital business revolution for the youth of our country by giving them the
+            required skills and knowledge. through which our youth can shape their career and make fortunes.</p>
+        <div class="row mt-5">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-4 cont-img">
+                <img src="image/winningimg15.jpg" alt="image" width="500px" height="450px" style="padding-bottom: 20px;" class="image">
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-4">
+                <h3 class="log" style="margin-bottom:20px; margin-top:70px; font-size: 20px; color: #2a60af; font-weight: bold;">Founder & CEO of WinningWavez</h3>
+                <h5 class="log1" style="margin-bottom:10px;font-size:22px; font-weight: bold;">Mr. Bikash Pradhan</h5>
+                <p style="text-align:justify">Bikash Pradhan ,the founder & CEO of winning wavez, a top e - learning
+                    platform focused on digital marketing skill courses . He's a key influencer of the company and
+                    experience in digital marketing field . Winning wavez vision is to empower people with digital
+                    knowledge, bridging education in an affordable rate . He always say that ,"don't miss the
+                    opportunity, your one step can make You successful ".</p>
+                <br>
+                <div class="col-lg-2"></div>
+            </div>
+        </div>
+</div>
+</section>
+
+
+
+<!-- WORK CULTURE AT WINNING WAVEZ -->
+<section class="py">
+    <div class="section1">
+        <h1 class="display">WORK CULTURE AT WINNING WAVEZ</h1>
+        <hr class="display1">
+    </div>
+    <div class="container">
+        <p class="paragraph">We are a company with values for life, vision for a future and a mission of growth.
+            We believe that each and everyone on the earth is blessed with equal potential and so they must get the equal
+            opportunity to grow as well. We believe in collaboration over competition and we are working towards
+            creating this supporting environment with our values for life. Because we believe that’s how we can create
+            this country with value where everyone helps everyone and contribute to the overall growth of the nation by
+            growing themselves.</p>
+        <div class="row">
+            <div class="col-lg-6">
+                <p class="paragraph1">Winning Wavez that provides job offers would likely be an
+                    institution dedicated not only to teaching digital skills but also to helping students secure employment in
+                    relevant fields. Such an academy might have partnerships with companies in the tech industry, digital
+                    marketing agencies, software development firms, and other organizations seeking individuals with digital
+                    expertise.To facilitate job placements, the academy may offer career services such as resume building,
+                    interview preparation, networking opportunities, and connections to potential employers. Additionally, the
+                    curriculum and training programs would likely be designed with industry demands in mind, ensuring
+                    that students graduate with the skills and knowledge needed to thrive in the job market.
+               </p>
+            </div>
+            <div class="col-lg-6 cont-img">
+                <img src="image/winningimg16.jpg" alt="image" width="636px" height="380px" style="border-radius: 7px;">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 mt-4 order-lg-1 cont-img">
+                <img src="image/winningimg17.jpg" alt="image" style="border-radius: 7px;">
+            </div>
+            <div class="col-lg-6 order-lg-2">
+                <p style="text-align: justify; padding-top: 54px;">
+                    Winning Wavez that extends job offers is probably a facility that is committed to helping students
+                    find work in fields that are relevant to their studies as well as teaching digital skills. Such an
+                    academy might collaborate with digital marketing and tech corporations.agencies, software development companies, and other businesses looking for someone with experience
+                    in the digital space.The school may provide career services including resume writing, interview practice, networking
+                    opportunities, and contacts to possible employers in order to help with job placements. Furthermore,
+                    the training programs and curriculum would probably be created with the needs of the sector in mind,
+                    guaranteeing that graduates have the abilities and know-how required to succeed in the labor market.
+                    Through the integration of instruction and assistance with job placement, this kind of academy seeks
+                    to close the gap between.Over the years.
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-6 order-lg-3">
+                <p style="text-align:justify; padding-top: 54px;">
+                    Winning Wavez sounds like an educational initiative focused on providing training and resources
+                    to enhance digital skills. It could cover a wide range of topics, such as programming, digital
+                    marketing, graphic design, data analysis, and more. These skills are increasingly valuable in
+                    today's job market, as technology continues to play a central role in many industries. Such an academy might
+                    offer online courses, workshops, seminars, or even certification programs to help individuals
+                    acquire and improve their digital skills.Bhubaneswar, the capital city of the eastern Indian state of Odisha, is not only renowned for its
+                    rich cultural heritage and architectural marvels but also for its burgeoning surfing scene. Over the
+                    years,Bhubaneswar has emerged as a hotspot for surfers, both seasoned professionals and enthusiastic
+                    beginners, drawn by its pristine beaches, favorable weather conditions, and the thrill of riding the
+                    waves.
+                </p>
+            </div>
+            <div class="col-lg-6 order-lg-4 cont-img">
+                <img src="image/winningimg18.jpg" alt="image"  width="636px" height="420px" style="border-radius: 7px;">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 order-lg-1 cont-img">
+                <img src="image/winningimg19.jpg" alt="image"  width="636px" height="420px" style="border-radius: 7px;">
+            </div>
+            <div class="col-lg-6 order-lg-2">
+                <p style="text-align:justify; padding-top: 54px;">
+                    The Winning Wavez provide career services including resume writing, interview practice, networking
+                    opportunities, and contacts to possible employers in order to help with job placements. Furthermore,
+                    the training programs and curriculum would probably be created with the needs of the sector in mind,
+                    guaranteeing that graduates have the abilities and know-how required to succeed in the labor market.
+                    Through the integration of instruction and assistance with job placement, this kind of academy seeks
+                    to close the gap between.Over the years, Bhubaneswar has emerged as a hotspot for surfers, both
+                    seasoned professionals and enthusiastic beginners, drawn by its pristine beaches, favorable weather
+                    conditions, and the thrill of riding the waves. In this comprehensive exploration, we delve into the
+                    captivating world of "Winning Wavez Bhubaneswar" and uncover the factors contributing to its growing
+                    popularity.In this comprehensive exploration, we delve into the captivating world of "Winning Wavez
+                    Bhubaneswar" and uncover the factors contributing to its growing popularity.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "footer.php" ?>
