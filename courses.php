acb0517562eaec584888fc20fea2a24e7ce84ed6
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:43:49 +0530

    Add files via upload

---
+++ courses.php
@@ -0,0 +1,171 @@
+<?php include "header.php" ?>
+<div class="course-heading" style="background-image:url('image/winningimg14.jpg'); height: 405px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" style="text-align: center; padding: 135px;">
+                <span class="demo"><a href="#">HOME</a> / COURSES</span>
+                <div class="heading">
+                <h3>COURSES</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="pct" style="padding:85px">
+<button type="button" class="btn btn-primary" id="btn1">Show All</button>
+<button type="button" class="btn btn-primary" id="btn2">Pre Win</button>
+<button type="button" class="btn btn-primary" id="btn2">Win Lite</button>
+<button type="button" class="btn btn-primary" id="btn2">Ultra Win</button>
+<button type="button" class="btn btn-primary" id="btn2">Win Master</button>
+</div>
+
+<div class="dp">
+  <div class="container">
+    <div class="row pc">
+      <div class="col-lg-4 offset-lg-4">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+          <div class="post-img kl"> <a href="courses"><img src="image/winningimg24.jpg" alt=""></a>
+          </div>
+          <h4 class="course-section"><a href="#">Mobile Video Editing</a><span>10 hour</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg25.jpg" alt=""></a>
+        </div>
+        <h4 class="course-section"><a href="#">Canva Mastery</a><span>10 hour</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+ 
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg26.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Sales Closing Mastery</a><span>10 hour</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+ 
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg27.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Spoken English</a><span>10 hour</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg28.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Ms Excel</a><span>9 hour</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg29.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Instagram Grow</a><span>10 hours</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg30.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Success in Online Business</a><span>10 hours</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg31.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Coding</a><span>10 hours</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg32.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Html</a><span>10 hours</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg33.jpg" alt=""></a>
+         </div>
+         <h4 class="course-section"><a href="#">Youtube Success Blueprint</a><span>10 hours</span></h4>
+          <div class="main-button">
+            <a href="contact">View More</a>
+          </div>
+        </div>
+      </div>
+      
+      <div class="container">
+<ul class="pagination d-flex justify-content-center">
+              <li class="disabled"><a href="#">«</a></li>
+              <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#">»</a></li>
+            </ul>           
+</div>   
+    </div>
+  </div>
+</div>
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "footer.php" ?>
