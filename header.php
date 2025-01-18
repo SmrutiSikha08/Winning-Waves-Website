c49739d6ee740cf608716d5fdac1832264185d62
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:39:43 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/winning.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="icon" type="image/x-icon" href="image/winning.jpg">
+    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <title>winning-waves</title>
+</head>
+<body>
+<section>
+<div class="winning">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6">
+          <div class="icon">          
+          <ul class="kyc">
+            <li><i class="bi bi-envelope"></i> supportwavez@gmail.com</li>
+            <li><i class="bi bi-telephone-fill"></i> +91-9668740823</li>
+          </ul>
+        </div>
+</div>
+        <div class="col-lg-6 col-md-6">
+          <ul class="social-links">
+            <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+            <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+            <li><a href="#"><i class="bi bi-whatsapp"></i></a></li>
+            <li><a href="#"><i class="bi bi-youtube"></i></a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+
+  <section>
+    <nav class="navbar navbar-expand-sm navbar-dark ">
+      <div class="container">
+        <img src="image/winning.jpg" alt="a" width="7%" height="70px" style="border-radius: 135px;"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar"></button>
+        <div class="collapse navbar-collapse justify-content-end" id="mynavbar">
+          <div class="d-flex">
+            <ul class="navbar-nav me-auto">
+              <li class="style">
+                <a class="nav-link  active" href="index.php">HOME</a>
+              </li>
+              <li>
+                <a class="nav-link" href="about.php">About Us</a>
+              </li>
+              <li>
+                <a class="nav-link" href="courses.php">Courses</a>
+              </li>
+              <li>
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+              <li>
+                <a class="nav-link" href="login.php">Login/Register</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+    </nav>
+  </section>
+
+  <script src="js/winning.js"></script>
+</body>
+</html>
