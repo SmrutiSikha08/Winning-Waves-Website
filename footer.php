f7c11d0c3b79020aaa26e41f2b11c4348d04421e
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:40:08 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,61 @@
+<section>
+  <footer class="footer-d">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3">
+        <div class="footer-col sp">
+          <img src="image/winning.jpg" width="7%" height="70px" alt="logo">
+          <ul>
+            <li><a href="#"><i class="bi bi-geo-alt-fill"></i>Bhubaneswar, Odisha</a></li>
+            <li><a href="#"><i class="bi bi-envelope-fill"></i>support@winningwavez.com</a></li>
+            <li><a href="#"><i class="bi bi-telephone"></i>+91 9668740823</a></li>
+          </ul>
+        </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="footer-col">
+          <h4>Get Help</h4>
+          <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Documents</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+          </ul>
+        </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="footer-col">
+          <h4>Digital Contents</h4>
+          <ul>
+            <li><a href="#">Download</a></li>
+            <li><a href="#">All Courses</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="footer-col">
+          <h4>Legal Terms</h4>
+          <ul>
+            <li><a href="#">Refund Policy</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+            <li><a href="#">Disclaimer</a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</section>
+
+<footer>
+  <div class="container">
+    <div class="col-lg-12 ivm">
+      <p style="text-align:center">COPYRIGHT ©2024 WINNINGWAVEZ.COM ALL RIGHTS RESERVED.
+      Designed By
+        <a href="#"> STRIVINGSQUAD PVT LTD</a>
+      </p>
+    </div>
+  </div>
+</footer>
