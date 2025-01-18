b5ce06debbaf9ceefed7cae176955791916bbe79
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:41:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,405 @@
+<?php include "header.php" ?>
+
+<!-- testimonial carousel section -->
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+  </div>
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/winningimg1.jpg" alt="a" class="d-block" style="width:100%" height="562px">
+      <div class="carousel-caption cap1">
+      <h1> Welcome To Winning Wavez</h1>
+        <h4>Scope Of Skills In Digital</h4>
+        <p style="font-size: 14px; color: white; line-height: 25px; font-weight: bolder;">
+          Empowering students through digital skills, the path to self-independence unfolds. By
+          embracing online
+          learning, students equip themselves with valuable tools for the digital era. Harnessing these skills, they
+          embark on entrepreneurial journeys, establishing businesses online
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/winningimg2.jpg" alt="a" class="d-block" style="width:100%" height="522px">
+      <div class="carousel-caption cap1">
+      <h1> Welcome To Winning Wavez</h1>
+        <h4>Scope Of Skills In Digital</h4>
+        <p style="font-size: 14px; color: white; line-height: 25px; font-weight: bolder;">
+          Empowering students through digital skills, the path to self-independence unfolds. By
+          embracing online
+          learning, students equip themselves with valuable tools for the digital era. Harnessing these skills, they
+          embark on entrepreneurial journeys, establishing businesses online
+        </p>
+      </div> 
+    </div>
+  </div>
+    <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<!-- what we provide section -->
+<div class="container pt-6 eds">
+  <div class="text-center mx-auto mb-5" style="max-width: 600px;">
+    <h1 class="display-5 mb-0 mt-5" style="font-size: 40px;
+    font-weight: 700;">What We Provide</h1>
+    <hr class="w-25 mx-auto bg-primary">
+  </div>
+</div>
+
+<div class="section7">
+    <div class="container eds pt-5">
+        <div class="div8 container  my-4">
+            <div class="owl-carousel owl-theme car ">
+
+                <div class="post-slide">
+                    <div class="post-content">
+                    <center><i class="bi bi-person-fill"></i>
+                        <h3 class="post-title">
+                            <a href="#">Access Anywhere</a>
+                        </h3>
+                        <p class="post-description">Internet's ubiquity transforms every space into a classroom, letting learning accompany you wherever life leads.</p>
+                     </center>
+                      </div>
+                </div>
+                
+                <div class="post-slide">
+                    <div class="post-content" style="padding: 80px;">
+                    <center><i class="bi bi-calendar2-week-fill"></i>
+                        <h3 class="post-title">
+                            <a href="#">Learn At Your Time</a>
+                        </h3>
+                        <p class="post-description">All our courses are online, enabling flexible learning at your convenience, <br>  tailored to your schedule.</p>
+                        </center>
+                      </div>
+                </div>
+
+                <div class="post-slide">
+                    <div class="post-content">
+                    <center><i class="bi bi-journal-richtext"></i>
+                        <h3 class="post-title">
+                            <a href="#">Huge Course Library</a>
+                        </h3>
+                        <p class="post-description">Explore our computer courses library, featuring a diverse range from fundamental basics to advanced skills.</p>
+                        </center>
+                      </div>
+                </div>
+
+                <div class="post-slide">
+                    <div class="post-content">
+                    <center><i class="bi bi-person-fill"></i>
+                        <h3 class="post-title">
+                            <a href="#">Access Anywhere</a>
+                        </h3>
+                        <p class="post-description">Internet's ubiquity transforms every space into a classroom, letting learning accompany you wherever life leads.</p>
+                        </center>
+                      </div>
+                </div>
+
+                <div class="post-slide">
+                    <div class="post-content" style="padding: 80px;">
+                    <center><i class="bi bi-calendar2-week-fill"></i>
+                        <h3 class="post-title">
+                            <a href="#">Learn At Your Time</a>
+                        </h3>
+                        <p class="post-description">All our courses are online, enabling flexible learning at your convenience, <br>  tailored to your schedule.</p>
+                        </center>
+                      </div>
+                </div>
+
+                <div class="post-slide">
+                    <div class="post-content">
+                    <center><i class="bi bi-journal-richtext"></i>
+                        <h3 class="post-title">
+                            <a href="#">Huge Course Library</a>
+                        </h3>
+                        <p class="post-description">Explore our computer courses library, featuring a diverse range from fundamental basics to advanced skills.</p>
+                        </center>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<!-- courses section -->
+<div class="dp">
+  <div class="container">
+    <div class="row pc">
+      <div class="col-lg-4 offset-lg-4">
+        <div class="rsc">
+          <h6>| Courses</h6>
+          <h2>Our Packages</h2>
+          <hr class="w-25 mx-auto bg-primary">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6">
+        <div class="item">
+          <div class="post-img kl"> <a href="courses"><img src="image/winningimg3.jpg" alt=""></a>
+          </div>
+          <span class="category">Pre Win</span>
+          <h4 class="course-catagory"><a href="#">Course Fees:</a><span>₹ 999</span></h4>
+          <div class="main-button">
+            <a href="contact">Enroll Now</a>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg5.jpg" alt=""></a>
+        </div>
+          <span class="category">Win Lite</span>
+          <h4 class="course-catagory"><a href="#">Course Fees:</a><span>₹ 2399</span></h4>
+          <div class="main-button">
+            <a href="contact">Enroll Now</a>
+          </div>
+        </div>
+      </div>
+ 
+      <div class="col-lg-3 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg4.jpg" alt=""></a>
+         </div>
+          <span class="category">Ultra Win</span>
+          <h4 class="course-catagory"><a href="#">Course Fees:</a><span>₹ 4999</span></h4>
+          <div class="main-button">
+            <a href="contact">Enroll Now</a>
+          </div>
+        </div>
+      </div>
+ 
+      <div class="col-lg-3 col-md-6">
+        <div class="item">
+        <div class="post-img kl"> <a href="courses"><img src="image/winningimg6.jpg" alt=""></a>
+         </div>
+          <span class="category">Win Master</span>
+          <h4 class="course-catagory"><a href="#">Course Fees:</a><span>₹ 7999</span></h4>
+          <div class="main-button">
+            <a href="contact">Enroll Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+<!-- faq section -->
+<div class="text-center mx-auto mb-5" style="max-width: 600px;">
+  <h1 class="display mb-0 mt-5">Frequently Asked Questionss</h1>
+  <hr class="w-25 mx-auto bg-primary">
+</div>
+
+<div class='faq'>
+  <input id='faq-a' type='checkbox'>
+  <label for='faq-a'>
+    <p class="faq-heading">What is WinningWavez?</p>
+    <div class='faq-arrow'></div>
+      <p class="faq-text">Winning wavez is an Ed-tech company that provides online courses focused on developing the skills and knowledge required for individuals to become successful entrepreneurs and investors.</p>
+  </label>
+  <input id='faq-b' type='checkbox'>
+  <label for='faq-b'>
+    <p class="faq-heading">Which courses winning wavez offer?</p>
+    <div class='faq-arrow'></div>
+      <p class="faq-text">Welcome to WinningWavez, a transformative educational hub crafted for both beginners and seasoned individuals. Immerse yourself in expert-designed courses covering a spectrum of skills, including marketing, sales, personal development, web development, online earning strategies, digital marketing, mobile video editing, diverse earning sources, and strategic trading insights. Our platform offers a comprehensive and interactive learning experience tailored for students who are eager to develop practical knowledge and skills. Benefit from expert guidance, apply your learning through hands-on projects, and stay up-to-date with regularly updated content. Join WinningWavez and embark on a journey of continuous evolution toward a prosperous future</p>
+  </label>
+  <input id='faq-c' type='checkbox'>
+  <label for='faq-c'>
+    <p class="faq-heading">How to enroll in our courses?</p>
+    <div class='faq-arrow'></div>
+      <p class="faq-text">Follow the steps to enroll in our courses.
+
+1. Go to WinningWavez Website
+
+2. Click on View More
+
+3. Fill in the asked details
+
+4. Choose your favorite course</p>
+  </label>
+  <input id='faq-d' type='checkbox'>
+  <label for='faq-d'>
+    <p class="faq-heading">Will i get the completetion certificate?</p>
+    <div class='faq-arrow'></div>
+      <p class="faq-text">Yes, you will get the completion certificate.</p>
+  </label>
+   </div>
+
+
+
+
+
+<!-- our achievement section -->
+<section class="text-center mx-auto mb-5 mt-5" style="max-width: 600px;">
+  <h1 class="display mb-0 mt-5">Our Achievements</h1>
+  <hr class="w-25 mx-auto bg-primary">
+</section>
+
+<div class="fun-facts">
+  <div class="container">
+    <div class="row lop">
+      <div class="col-lg-12">
+        <div class="wrapper">
+          <div class="row cop">
+            <div class="col-lg-3">
+             <div class="counter">
+                <p>Courses</p><br>
+                <h2 class="timer count-title count-number" data-to="10" data-speed="1000">10</h2>
+
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="counter">
+                <p>Live Trainings</p><br>
+                <h2 class="timer count-title count-number" data-to="60" data-speed="1000">60</h2>
+              </div>
+            </div>
+
+            <div class="col-lg-3">
+              <div class="counter" style="padding: 32px">
+                <p>Enrolled Learners</p>
+                <h2 class="timer count-title count-number" data-to="720" data-speed="1000">720</h2>
+              </div>
+            </div>
+
+            <div class="col-lg-3">
+              <div class="counter">
+                <p>Satisfaction Rate</p><br>
+                <h2 class="timer count-title percentage" data-to="99" data-speed="1000">99</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<!-- testimonial carousel section -->
+<div class="container my-5">
+
+  <h2 class="text-center">Testimonials</h2>
+
+
+
+  <section id="testim" class="testim">
+
+    <!--         <div class="testim-cover"> -->
+    <div class="wrap">
+
+      <span id="right-arrow" class="arrow right fa fa-chevron-right"></span>
+      <span id="left-arrow" class="arrow left fa fa-chevron-left "></span>
+      <ul id="testim-dots" class="dots">
+        <li class="dot active"></li><!--
+                    -->
+        <li class="dot"></li><!--
+                    -->
+        <li class="dot"></li><!--
+                    -->
+        <li class="dot"></li><!--
+                    -->
+        <li class="dot"></li><!--
+                    -->
+        <li class="dot"></li><!--
+                    -->
+        <li class="dot"></li>
+      </ul>
+      <div id="testim-content" class="cont">
+
+        <div class="active">
+          <div class="img"><img src="image/winningimg7.jpg" alt=""></div>
+          <h2>Soumya Ranjan Acharya</h2>
+          <p>I want to say that Winning Wavez is the best company in the whole Ed-tech industry.</p>
+        </div>
+
+        <div>
+          <div class="img"><img src="image/winningimg8.jpg" alt=""></div>
+          <h2>Soubhagya Sahoo</h2>
+          <p>
+            I am glad to share my experience with you regarding this platform. Here, I have been taught to create
+            enormous opportunities.It's the best decision of my life.
+          </p>
+        </div>
+        <div>
+          <div class="img"><img src="image/winningimg9.jpg" alt=""></div>
+          <h2>Subhasis Swain</h2>
+          <p>This platform provided me so many skills through its courses. Thank you so much for changing
+              my life.</p>
+        </div>
+
+        <div>
+          <div class="img"><img src="image/winningimg10.jpg" alt=""></div>
+          <h2>Namrata Ojha</h2>
+          <p>Now, I understood the advantages of Digital
+            Marketing to create my brand. It has created as power to grow forward. Indeed, it is one of the best
+            decisions in my life.</p>
+        </div>
+
+        <div>
+          <div class="img"><img src="image/winningimg11.jpg" alt=""></div>
+          <h2>Bibhash Ranjan Panda </h2>
+          <p>Here, you can learn Ads Marketing, Social Media Marketing, Public Dealing, and Communication skills. I
+            highly
+            recommend everyone to explore it.</p>
+        </div>
+        <div>
+          <div class="img"><img src="image/winningimg12.jpg" alt=""></div>
+          <h2>Soumya Shree Barik </h2>
+          <p>Here, I got ample opportunities for learning and growth. The learning environment as also supportive and
+          </p>
+        </div>
+        <div>
+          <div class="img"><img src="image/winningimg13.jpg" alt=""></div>
+          <h2>Kumkum Ransingh</h2>
+          <p>I was struggling to make extra income, then I found Winning Wavez Private Limited. Thank you so much
+            Winning Wavez
+            Private Limited for this life-changing opportunity.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+<script src="https://use.fontawesome.com/1744f3f671.js"></script>
+<?php include "footer.php" ?>
+
+
+
+
+
+
+
+
