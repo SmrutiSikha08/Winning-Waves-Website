ab45c961921bfc88a2db6d6de7d19f82a60d55ae
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:44:16 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/winning.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Login Page</title>
+</head>
+<body style="background-color : lightgray;">
+    <div class="login">
+<center><img src="image/winning.jpg" alt="logo" width="100px" hight="50px"> </center> 
+<center style="font-weight: bold; font-size: 20px;">Login</center>
+    <form>
+        <div class="container" style="text-align: center;"> 
+            <br>
+            <div class="input-container">
+            <i class= "bi bi-people-fill icon1"></i>
+            <input type="text" class="input-field" placeholder="Enter Username" name="username" required>
+            </div>
+
+                      
+            <div class="input-container">
+    <i class="bi bi-key icon1"></i>
+    <input class="input-field" type="password" placeholder="Password" name="psw">
+  </div>
+
+  <button type="submit" class="btn15">Log in</button>
+            <p class="sign">Don't have an account ? <span style="color:#3264bf"><b>Sign up now</b></span></p>  
+        </div> 
+    </form> 
+    </div>  
+</body>
+</html>
