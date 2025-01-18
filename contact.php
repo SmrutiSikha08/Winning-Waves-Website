040311bbcb19b1aa35753b88a014e041eafbb53e
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 21:42:48 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,86 @@
+<?php include "header.php" ?>
+<div class="course-heading" style="background-image:url('image/winningimg14.jpg'); height: 405px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" style="text-align: center; padding: 135px;">
+                <span class="demo"><a href="#">HOME</a> / CONTACT US</span>
+                <div class="heading">
+                <h3>CONTACT US</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="contact-page section" style="margin-bottom: 60px;padding: 9px;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="section-heading">
+          <h6>| Contact Us</h6>
+          <h2>Get In Touch</h2>
+        </div>
+        <p>Such an academy might offer online courses, workshops, seminars, or even certification programs to help
+          individuals acquire and improve their digital skills .
+        <br>We love hearing from you! Whether you have a question about our products, need assistance with an order, or simply want to say hello, we're here to help.</p>
+        <div class="row">
+          <div class="col-lg-10">
+            <div class="item phone">
+              <img src="image/winningimg21.jpg" alt="" style="max-width: 31px;">
+              <h6>
+                Bhubaneswar, Odisha<br><span>Address</span></h6>
+            </div>
+          </div>
+          <div class="col-lg-2"></div>
+          <div class="col-lg-10">
+            <div class="item phone">
+              <img src="image/winningimg22.jpg" alt="" style="max-width: 38px;">
+              <h6>9668740823<br><span>Phone Number</span></h6>
+            </div>
+          </div>
+          <div class="col-lg-2"></div>
+          <div class="col-lg-10">
+            <div class="item email">
+              <img src="image/winningimg23.jpg" alt="" style="max-width: 38px;">
+              <h6>support@winningwavez.com<br><span>Business Email</span></h6>
+            </div>
+          </div>
+          <div class="col-lg-2"></div>
+        </div>
+      </div>
+      <div class="col-lg-5">
+        <form id="contact-form" action="" method="post">
+          <div class="row">
+            <div class="col-lg-12">
+              <fieldset>
+                <label for="name">Full Name</label>
+                <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required="">
+              </fieldset>
+            </div>
+
+            <div class="col-lg-12">
+              <fieldset>
+                <label for="email">Email Address</label>
+                <input type="text" name="email" id="email" placeholder="Your E-mail..." required="">
+              </fieldset>
+            </div>
+            
+            <div class="col-lg-12">
+              <fieldset>
+                <label for="message">Message</label>
+                <textarea name="message" id="message" placeholder="Your Message"></textarea>
+              </fieldset>
+            </div>
+            <div class="col-lg-12">
+              <div class="icon-button">
+                <a href="property-details.html"><i class="bi bi-calendar-fill"></i>Send Message</a>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="col-lg-1"></div>
+    </div>
+  </div>
+</div>
+<?php include "footer.php" ?>
